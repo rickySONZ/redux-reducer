7b0bc3a6ddb7aabd6faabf641834378c934d3a69
Ryan Erricson <ryanerricson@gmail.com>
2021-06-08 14:32:23 -0400

    commit tree bc14d226814e82ebb12b2e8903097a95e6f72c7e

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+
+    let existingFriends = {friends: []}
+    let friend = action.friend
+    state.friends.map ( friend => existingFriends.friends.push(Object.assign({}, friend)))
+    switch(action.type){
+    case 'ADD_FRIEND':
+        existingFriends.friends.push(Object.assign({}, friend))
+        return existingFriends
+    case 'REMOVE_FRIEND':
+        existingFriends.friends = existingFriends.friends.filter(friend => friend.id !== action.id)
+        return existingFriends
+    default:
+        return state;
+    }
 }
